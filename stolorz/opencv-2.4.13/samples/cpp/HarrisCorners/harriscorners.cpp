9983b2b1935304044f93bcf3a7da0748910025f3
Kieran Stolorz <stolorz@usc.edu>
2017-03-22 20:27:48 -0700

    small stuff, comments on how to get corner consensus

--- stolorz/opencv-2.4.13/samples/cpp/HarrisCorners/harriscorners.cpp
+++ stolorz/opencv-2.4.13/samples/cpp/HarrisCorners/harriscorners.cpp
@@ -3,13 +3,15 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <vector>
+#include <math.h>
 
 using namespace cv;
 using namespace std;
 
 // Global variables
 Mat src, src_gray;
-int thresh = 200;
+int thresh = 100;
 int max_thresh = 255;
 
 char* source_window = "Source image";
@@ -17,6 +19,36 @@ char* corners_window = "Corners detected";
 
 // Function header
 void cornerHarris_demo( int, void* );
+
+
+//returns distance between p1 and p2
+double distance(Point p1, Point p2){
+  return sqrt((p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y));
+}
+
+
+//averages the corners in array to one x,y
+
+/*
+@TODO we need this avg corners function to be able to coagulate all the nearby corner detections
+into the proper mathematical center
+
+we need a case/switch statement for if we pick up 1, 2, 3, 4, 5, ... corners next to each other to get
+the true "single" corner between them. 
+
+
+
+*/
+
+void avgCorners(vector<Point> v){
+  printf("in this thang\n");
+  for (int i = 0; i < v.size(); i++){
+    printf("distance from (%d,%d) to (%d,%d) is %f\n", v[i].x, v[i].y, v[i+1].x, v[i+1].y,distance(v[i], v[i+1]));
+  }
+}
+
+
+
 
 /** @function main */
 int main( int argc, char** argv )
@@ -52,6 +84,8 @@ int main( int argc, char** argv )
 
   cornerHarris_demo( 0, 0 );
 
+
+
   waitKey(0);
   return(0);
 }
@@ -67,6 +101,7 @@ void cornerHarris_demo( int, void* )
   int blockSize = 2;
   int apertureSize = 3;
   double k = 0.04;
+  vector<Point> cornerVec;
 
   /// Detecting corners
   cornerHarris( src_gray, dst, blockSize, apertureSize, k, BORDER_DEFAULT );
@@ -82,9 +117,19 @@ void cornerHarris_demo( int, void* )
             if( (int) dst_norm.at<float>(j,i) > thresh )
               {
                circle( dst_norm_scaled, Point( i, j ), 5,  Scalar(0), 2, 8, 0 );
+               cornerVec.push_back(Point(i,j));
               }
           }
      }
+
+
+
+
+
+
+  //average the corners into one x,y
+  avgCorners(cornerVec);
+
   /// Showing the result
   namedWindow( corners_window, CV_WINDOW_AUTOSIZE );
   imshow( corners_window, dst_norm_scaled );
